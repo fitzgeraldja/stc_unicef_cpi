c7e56772dd3965cf0b8777e7452fbdab218a6137
Daniela Pinto Veizaga <danielapintoveizaga@gmail.com>
2022-07-06 12:39:11 -0400

    adds general funcs

---
+++ src/utils/general.py
@@ -0,0 +1,24 @@
+import yaml
+
+
+def read_yaml_file(yaml_file):
+    """Load yaml configurations"""
+    config = None
+    try:
+        with open(yaml_file, "r") as f:
+            config = yaml.safe_load(f)
+    except:
+        raise FileNotFoundError("Couldnt load the file")
+
+    return config
+
+
+def get_facebook_credentials(creds_file):
+    """Get credentials for accessing FB API from the credentials file"""
+    creds = read_yaml_file(creds_file)["facebook"]
+    token = creds["access_token"]
+    id = creds["account_id"]
+    limit = creds["limit"]
+    radius = creds["radius"]
+
+    return token, id, limit, radius
