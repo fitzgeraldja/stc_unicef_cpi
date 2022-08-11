f0d969986c43d2e5bb1ab0a1b7d4fa4129c4326b
Daniela Pinto Veizaga <danielapintoveizaga@gmail.com>
2022-08-11 17:55:53 -0400

    minor edits

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -4,20 +4,20 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-#trainig data endpoint
-@app.route('/api/data', methods=['GET', 'POST', 'DELETE'])
+
+@app.route('/api/data', methods=['GET'])
 def retrieve_training_data():
     # gets all users
-    if request.method=='GET':
-
+    if request.method == 'GET':
         df = pd.read_csv('../data/interim/hexes_nigeria_res7_thres30.csv')
+        df = df.head(1000)
+        return {'data': df.to_dict()}, 200
 
-        return {'df': df.to_dict()}, 200
 
-#micro-estimate endpoint
 @app.route('/api/estimates', methods=['GET'])
 def retrieve_microestimates_data():
     pass
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='0.0.0.0', port=105, debug=True)
